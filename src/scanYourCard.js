78b0f33d9989df75add18c3c7249ae018c55c5f9
martinIvovIv <martinivoviv@gmail.com>
2025-10-17 17:44:37 +0200

    fix: disable timeout redirect if already redirected

--- src/scanYourCard.js
+++ src/scanYourCard.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import Results from './Results';
-import { useRouteChange } from './navigation';
-const ScanYourCard = ({ direction = 'left', result }) => {
-    const routeChange = useRouteChange();
-    setTimeout(() => {
-        routeChange('/');
-    }, 5000);
+import { useRef, useEffect } from "react";
+import Results from "./Results";
+import { useRouteChange } from "./navigation";
+const ScanYourCard = ({ direction = "left", result }) => {
+  const routeChange = useRouteChange();
 
-    return (
-        <div className="scan-your-card">
-            <Results result={result} />
-            <img src="../assets/blue_arrow.webp" alt="scan your card" className={`direction-${direction}`} onClick={() => routeChange('/')} />
-        </div>
-    );
+  const timeoutRef = useRef(null);
+
+  useEffect(() => {
+    timeoutRef.current = setTimeout(() => routeChange("/"), 5000);
+    return () => {
+      timeoutRef.current && clearTimeout(timeoutRef.current);
+    };
+  }, []); // eslint-disable-line
+
+  const handleRedirect = () => {
+    timeoutRef.current && clearTimeout(timeoutRef.current);
+    routeChange("/");
+  };
+
+  return (
+    <div className="scan-your-card">
+      <Results result={result} />
+      <img
+        src="../assets/blue_arrow.webp"
+        alt="scan your card"
+        className={`direction-${direction}`}
+        onClick={() => handleRedirect()}
+      />
+    </div>
+  );
 };
 
 export default ScanYourCard;
